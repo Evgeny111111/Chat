b72abadd70dc5fec9ba21b51b9cbb0186c955c01
Evgeny111111 <rasshivalov95@mail.ru>
2025-03-21 15:06:39 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://www.creativefabrica.com/wp-content/uploads/2018/11/Kids-Logo-by-Acongraphic-76.jpg"></img>
+    </header>
+  );
+};
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://sun9-68.userapi.com/impf/mi7vSURe-46qz7-wzYT97_-nsjI3lv2KrHzBrQ/oEj1bmfAV1s.jpg?size=1920x768&quality=95&crop=562,0,1214,485&sign=6cf3f585802c557283a9951f95eb43ef&type=cover_group" />
+      </div>
+      <div>ava +desc</div>
+      <div>
+        My posts
+        <div>NewPost</div>
+      </div>
+      <div>post</div>
+      <div>post2</div>
+    </div>
+  );
+};
+
+export default Profile;
